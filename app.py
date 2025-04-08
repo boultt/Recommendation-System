65f33cdd8adbc3940b34c2c237246aa19385a210
Aarsh-37 <vaidyaab@rknec.edu>
2025-04-08 16:14:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,10 @@ def recommend_songs(song_name, df, num_recommendations=5):
     return recommendations
 
 # --- Streamlit UI ---
-st.title("🎵 Music Recommendation System with Spotify")
+st.set_page_config(page_title="Vibe - Music Recommender", layout="wide")
+
+st.markdown("<h1 style='font-size: 3rem; color:#1DB954;'>VibeVerse</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='margin-top:-20px;'>Music Recommender System</h3>", unsafe_allow_html=True)
 
 song_input = st.selectbox("Choose a song:", sorted(df["name"].unique()))
 
